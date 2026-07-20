d3e33dc5fcb6569b8e2732d92f510fdb6ce6d8ce
Debanshu Jena <jenadebanshu419@gmail.com>
2026-07-20 19:30:17 +0530

    Implemented a Python-based Guess the Number game using the random module, loops, conditional statements, and user input. Added attempt tracking and input validation.

---
+++ guess_the_number.py
@@ -0,0 +1,35 @@
+import random
+
+print("===================================")
+print("     GUESS THE NUMBER GAME")
+print("===================================")
+
+# Generate a random number between 1 and 100
+secret_number = random.randint(1, 100)
+
+attempts = 0
+
+print("I have selected a number between 1 and 100.")
+print("Can you guess it?")
+
+while True:
+    try:
+        guess = int(input("Enter your guess: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Too Low! Try Again.\n")
+
+        elif guess > secret_number:
+            print("Too High! Try Again.\n")
+
+        else:
+            print("\n🎉 Congratulations!")
+            print(f"You guessed the correct number: {secret_number}")
+            print(f"You took {attempts} attempts.")
+            break
+
+    except ValueError:
+        print("Please enter a valid number!")
+
+print("\nThank you for playing!")
